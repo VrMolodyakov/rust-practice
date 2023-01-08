30a19b1406a1deecc3b887c2d21dd7d3111f7518
Vyacheslav Molodyakov <vrmolodyakov@mail.ru>
2023-01-08 18:03:53 +0300

    trait version

--- src/strsplit.rs
+++ src/strsplit.rs
@@ -1,10 +1,15 @@
-pub struct StrSplit<'a>{
-    remainder:Option<&'a str>,
-    delimiter:&'a str
+pub trait Delimeter{
+    fn next_delim(&self,s:&str) -> Option<(usize,usize)>;
 }
 
-impl <'a> StrSplit<'a>{
-    pub fn new(str:&'a str,delimiter:&'a str) ->Self{
+#[derive(Debug)]
+pub struct StrSplit<'r,D>{
+    remainder:Option<&'r str>,
+    delimiter:D,
+}
+
+impl <'r,D> StrSplit<'r,D>{
+    pub fn new(str:&'r str,delimiter:D) ->Self{
         Self{
             remainder:Some(str),
             delimiter
@@ -12,22 +17,51 @@ impl <'a> StrSplit<'a>{
     }
 }
 
-impl<'a> Iterator for StrSplit<'a>{
-    type Item = &'a str;
+impl<'r,D> Iterator for StrSplit<'r,D> 
+where 
+    D:Delimeter,
+{
+    type Item = &'r str;
     fn next(&mut self) ->Option<Self::Item>{
-       if let Some(ref mut remainder) = self.remainder{
-            if let Some(next_pos) = remainder.find(self.delimiter){
-                let until = &remainder[..next_pos];
-                *remainder = &remainder[(next_pos + self.delimiter.len())..];
-                Some(until)
-            }else{
-                self.remainder.take()
-            }
-       }else{
-            None
-       }
+        let remainder = self.remainder.as_mut()?;
+        if let Some((start,end)) = self.delimiter.next_delim(remainder){
+            let until = &remainder[..start];
+            *remainder = &remainder[end..];
+            Some(until)
+        }else{
+            self.remainder.take()
+        }
+      
     }
  }
+
+impl Delimeter for &str{
+    fn next_delim(&self,s:&str) -> Option<(usize,usize)>{
+        s.find(self).map(|start| (start,start + self.len()))
+    }
+}
+
+
+impl Delimeter for char{
+    fn next_delim(&self,s:&str) -> Option<(usize,usize)>{
+        s.char_indices()
+        .find(|(_,c)| c == self)
+        .map( |(start,_)| (start,start + self.len_utf8()))
+    }
+}
+
+
+
+fn until_char(str:&str,c:char) -> &'_ str{
+    return StrSplit::new(str,c)
+            .next()
+            .expect("at least something should be returned")
+}
+
+#[test]
+fn until_char_test(){
+    assert_eq!(until_char("goode", 'e'),"good");
+}
 
 #[test]
 fn it_work(){
